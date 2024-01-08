8e73b90d534f3e55b8d6eaf59d43a26f77f78112
agnesvarga1 <agikolyok@gmail.com>
2024-01-08 17:42:35 +0100

    bigletto creato dynamicly

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ const bigliettoKm = document.querySelector("#kmJourney");
 const ageHtml = document.querySelector("#agePassenger");
 const btnGenBigletto = document.querySelector("#genera");
 const btnAnnulla = document.querySelector("#delete");
+const containerBiglietto = document.querySelector(".biglietto-container");
+const nameDisplay = document.querySelector("#pass-nome");
+const bigliettoType = document.querySelector("#ticket-type-display");
+const carrDisplay = document.querySelector("#carr-display");
+const codeDisplay = document.querySelector("#code-display");
+const prezzoDisplay = document.querySelector("#prezzo-display");
 
 const prezzoKm = 0.21;
 let prezzoBiglietto,
@@ -13,14 +19,19 @@ let prezzoBiglietto,
   randomcodeCP;
 
 btnGenBigletto.addEventListener("click", function () {
+  containerBiglietto.classList.remove("noshow");
   namePassenger = namePassengerHtml.value;
   kmViaggio = bigliettoKm.value;
   etaPassenger = ageHtml.value;
   console.log(namePassenger);
   generaBiglettoPrezzo(kmViaggio, etaPassenger);
   console.log(prezzoBiglietto);
-  randomCarr = generaRandomNumber(1, 11);
+  nameDisplay.innerHTML = namePassenger;
+  randomCarr = generaRandomNumber(1, 15);
   randomcodeCP = generaRandomNumber(0, 99999);
+  carrDisplay.innerHTML = randomCarr;
+  codeDisplay.innerHTML = randomcodeCP;
+  prezzoDisplay.innerHTML = prezzoBiglietto;
 });
 
 btnAnnulla.addEventListener("click", deleteAll);
@@ -29,6 +40,9 @@ function generaBiglettoPrezzo(kms, eta) {
   prezzoBiglietto = (kms * prezzoKm).toFixed(2);
   const minDiscount = prezzoBiglietto * 0.2;
   const pensDiscount = prezzoBiglietto * 0.4;
+  eta === "minorenne" || eta === "pensionato"
+    ? (bigliettoType.innerHTML = `Bigletto Ridotto`)
+    : 0;
   if (eta === "minorenne") {
     prezzoBiglietto = (prezzoBiglietto - minDiscount).toFixed(2);
 
