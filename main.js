4eda983d5c8274cfe207a90c92b02515a0f25df3
agnesvarga1 <agikolyok@gmail.com>
2024-01-08 16:09:37 +0100

    genera prezzo bigletto

--- main.js
+++ main.js
@@ -4,28 +4,28 @@ const ageHtml = document.querySelector("#agePassenger");
 const btnGenBigletto = document.querySelector("#genera");
 
 const prezzoKm = 0.21;
-let prezzoBiglietto, namePassenger, kmViaggio, eta;
+let prezzoBiglietto, namePassenger, kmViaggio, etaPassenger;
 
 btnGenBigletto.addEventListener("click", function () {
   namePassenger = namePassengerHtml.value;
   kmViaggio = bigliettoKm.value;
-  eta = ageHtml.value;
+  etaPassenger = ageHtml.value;
   console.log(namePassenger);
-  console.log(kmViaggio);
-  console.log(eta);
+  generaBiglettoPrezzo(kmViaggio, etaPassenger);
+  console.log(prezzoBiglietto);
 });
 
-// function generaBiglettoPrezzo(kms, eta) {
-//   prezzoBiglietto = kms * prezzoKm;
-//   const minDiscount = prezzoBiglietto * 0.2;
-//   const pensDiscount = prezzoBiglietto * 0.4;
-//   if (eta === "minorenne") {
-//     prezzoBiglietto = prezzoBiglietto - minDiscount;
-//     return prezzoBiglietto;
-//   } else if (eta === "pensionato") {
-//     prezzoBiglietto = prezzoBiglietto - pensDiscount;
-//     return prezzoBiglietto;
-//   } else {
-//     return prezzoBiglietto;
-//   }
-// }
+function generaBiglettoPrezzo(kms, eta) {
+  prezzoBiglietto = (kms * prezzoKm).toFixed(2);
+  const minDiscount = prezzoBiglietto * 0.2;
+  const pensDiscount = prezzoBiglietto * 0.4;
+  if (eta === "minorenne") {
+    prezzoBiglietto = (prezzoBiglietto - minDiscount).toFixed(2);
+    return prezzoBiglietto;
+  } else if (eta === "pensionato") {
+    prezzoBiglietto = (prezzoBiglietto - pensDiscount).toFixed(2);
+    return prezzoBiglietto;
+  } else {
+    return prezzoBiglietto;
+  }
+}
